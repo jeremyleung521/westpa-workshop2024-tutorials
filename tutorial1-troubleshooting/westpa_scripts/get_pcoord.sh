3b62a97dd7895ce3b1c52d29e2437c312212b6c0
Jeremy M. G. Leung <jml230@pitt.edu>
2024-05-14 15:20:38 -0400

    salted init

--- tutorial1-troubleshooting/westpa_scripts/get_pcoord.sh
+++ tutorial1-troubleshooting/westpa_scripts/get_pcoord.sh
@@ -2,30 +2,25 @@
 
 cd $WEST_SIM_ROOT/ntl9_folding_synd
 
-# >>>>> Make temporary files <<<<<
-PCOORD=$(mktemp)
-TEMP_NAME=$(basename $(mktemp -u))
-
 # Grab Cluster ID
 CLUSTERID=$(basename $WEST_STRUCT_DATA_REF)
- 
+
 # >>>>> Calculate Pcoord <<<<<
 # This takes in a cluster number as argument and returns the pcoord.
-python map_pcoord.py $CLUSTERID > $PCOORD
- 
+python map_pcoord.py $CLUSTERID > pcoord.dat
+
 # >>>>> Backmap for a coordinate file <<<<<
 # This takes in cluster number and file name as arguments
 # and outputs a {file name}.nc coordinate file.
-python map_coord.py $CLUSTERID $TEMP_NAME
+python map_coord.py $CLUSTERID seg
 
 # >>>>> Return Pcoord <<<<<
-cat $PCOORD > $WEST_PCOORD_RETURN
-rm $PCOORD
- 
+cat pcoord.dat > $WEST_PCOORD_RETURN
+
 # >>>>> For HDF5 Framework <<<<<
 cp ntl9.pdb $WEST_TRAJECTORY_RETURN
-cp $TEMP_NAME.nc $WEST_TRAJECTORY_RETURN/seg.nc
-rm $TEMP_NAME.nc
+cp seg.nc $WEST_TRAJECTORY_RETURN/seg.nc
+rm seg.nc
 
 # cp ntl9.pdb $WEST_RESTART_RETURN
 cat $CLUSTERID > $WEST_RESTART_RETURN/parent.txt
