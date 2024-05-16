42f9916683376e0eeb4886b7295792a2c49f2437
Jeremy M. G. Leung <jml230@pitt.edu>
2024-05-16 18:01:07 -0400

    update tut3b

--- tutorial3b-lpath/run_lpath.sh
+++ tutorial3b-lpath/run_lpath.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # assign source and target states using the phi/psi angles from WE simulations
-lpath discretize -we -W ./west.h5 --assign-arguments "--config-from-file --scheme CRUDE_RMSD"
+#lpath discretize -we -W ./west.h5 --assign-arguments "--config-from-file --scheme CRUDE_RMSD" --debug
 
 # extract all successful pathways connecting source and target states
 # also extract the cluster labels for matching in the next step
-lpath extract -we -W ./west.h5 -A ./ANALYSIS/CRUDE_RMSD/assign.h5 -ss 1 -ts 0 -p --stride 10
+#lpath extract -we -W ./west.h5 -A ./ANALYSIS/CRUDE_RMSD/assign.h5 -ss 1 -ts 0 -p --stride 10
 
 # perform matching with condensing repeat pairs
 # uses the cluster labels as states through reassign_custom
+lpath match -we --condense 2 -pto 0 --n-clusters 1 --plots-hide
 #lpath match -we -ra reassign_custom.reassign_custom -op succ_traj/reassigned.pickle --condense 2
