0ae3c92ecbcbfe0a8fb2dd9f7755143acd936bf7
Jeremy M. G. Leung <jml230@pitt.edu>
2024-06-03 13:01:53 -0400

    update run_lpath.sh

--- tutorial2b-lpath/run_lpath.sh
+++ tutorial2b-lpath/run_lpath.sh
@@ -6,7 +6,7 @@ lpath discretize -we -W ./west.h5 --assign-arguments "--config-from-file --schem
 
 # extract all successful pathways connecting source and target states
 # also extract the cluster labels for matching in the next step
-lpath extract -we -W ./west.h5 -A ./ANALYSIS/CRUDE_RMSD/assign.h5 -ss 1 -ts 0 -p --aux sasa --stride 10 --stats
+lpath extract -we -W ./west.h5 -A ./ANALYSIS/CRUDE_RMSD/assign.h5 -ss 1 -ts 0 -p --aux sasa --stride 10
 
 # perform matching with condensing repeat pairs
 # uses the cluster labels as states through reassign_custom
