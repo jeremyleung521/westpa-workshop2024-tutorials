6f03652996c38851a7e4fa92002069f16397a913
sagarkania <55198807+sagarkania@users.noreply.github.com>
2024-06-10 17:09:21 -0700

    Delete tutorial3-rates/msm_we-edited directory

--- tutorial3-rates/msm_we-edited/HISTORY.rst
+++
@@ -1,8 +0,0 @@
-=======
-History
-=======
-
-0.1.0 (2021-03-02)
-------------------
-
-* Initial pre-release of existing code in restructured repo
--- tutorial3-rates/msm_we-edited/msm_we/_hamsm/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._clustering import ClusteringMixin
-from ._dimensionality import DimensionalityReductionMixin
-from ._analysis import AnalysisMixin
-from ._data import DataMixin
-from ._fluxmatrix import FluxMatrixMixin
-from ._plotting import PlottingMixin
